b867a2066ce1531b587862e9ad897e0f2af80c1d
nailyk-fr <nailyk_git@nailyk.fr>
2025-05-26 19:01:59 +0200

    Create a script to ease first install
    Signed-off-by: nailyk-fr <nailyk_git@nailyk.fr>

---
+++ setup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+## Use with : `curl -s https://raw.github.com/nailyk-fr/dot/setup/setup.sh | bash`
+
+cd
+mv .bashrc .bashrc.orig
+wget "https://raw.github.com/nailyk-fr/dot/master/.zshrc" -O .zshrc
+wget "https://raw.github.com/nailyk-fr/dot/master/.bashrc" -O .bashrc
+wget "https://raw.github.com/nailyk-fr/dot/master/prompt_nailyk_setup" -O prompt_nailyk_setup
+wget "https://raw.github.com/nailyk-fr/dot/master/.screenrc" -O .screenrc
+wget "https://raw.github.com/nailyk-fr/dot/master/.vimrc" -O .vimrc
+sudo ln -sf ~/.bashrc /root/
+sudo ln -sf ~/.vimrc /root/
+sudo ln -sf ~/.zshrc /root/
+sudo ln -sf ~/prompt_nailyk_setup /root/
+sudo chsh -s /bin/zsh $USER
+sudo chsh -s /bin/zsh root
